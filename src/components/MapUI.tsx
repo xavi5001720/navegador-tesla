5c3a2c7c7bdb53eac05d03af198908e721d218d9
Xavi <xavi.baeza@hotmail.com>
2026-04-06 00:12:56 +0200

    Hide offline friends from map 🚗💨🟢

--- src/components/MapUI.tsx
+++ src/components/MapUI.tsx
@@ -544,8 +544,8 @@ export default function MapUI({
         {gasStations.map(station => <Marker key={`gas-${station.id}`} position={[station.lat, station.lon]} icon={gasStationIcon} eventHandlers={{ click: () => { if (onGasStationClick) onGasStationClick(station); } }} />)}
         {weatherPoints.map(wp => <Marker key={`weather-${wp.id}`} position={[wp.lat, wp.lon]} icon={createWeatherIcon(wp.temp, wp.condition)} interactive={false} />)}
         
-        {/* Amigos (Markers Interactivos) */}
-        {friends.filter(f => f.last_lat && f.last_lon).map((friend) => (
+        {/* Amigos (Markers Interactivos - Solo Online) */}
+        {friends.filter(f => f.is_online && f.last_lat && f.last_lon).map((friend) => (
           <Marker 
             key={`friend-${friend.id}`} 
             position={[friend.last_lat!, friend.last_lon!]} 
