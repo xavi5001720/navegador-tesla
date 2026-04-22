20c7de4eb06f8b6782676d6dc125f8a2b2069c61
Xavi <xavi.baeza@hotmail.com>
2026-04-23 00:51:22 +0200

    fix: apply crossOrigin={true} to map tile layers to resolve ORB blocking errors

--- src/components/MapUI.tsx
+++ src/components/MapUI.tsx
@@ -1161,6 +1161,7 @@ function MapUI(props: MapUIProps) {
           <TileLayer 
             attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community" 
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" 
+            crossOrigin={true}
             eventHandlers={{
               tileerror: () => {
                 errorCountRef.current++;
@@ -1178,6 +1179,7 @@ function MapUI(props: MapUIProps) {
           <TileLayer 
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            crossOrigin={true}
           />
         )}
 
